e8c73d5d75e1425aa7518d1ffe87c97ec680c048
kbumsoo05 <kbumsoo12@gmail.com>
2024-04-25 20:38:18 +0900

    최신 파일 및 경로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>,
-)
+  </QueryClientProvider>
+);
